00af33daec80ec53c2e40dabdb8803d131237a3f
Anjumr19 <anju@newagesmb.com>
2024-02-09 12:46:31 +0530

    second commit

--- src/test/java/com/newage/vaadinDemo/UserInterface.java
+++ src/test/java/com/newage/vaadinDemo/UserInterface.java
@@ -1,10 +1,17 @@
 package com.newage.vaadinDemo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class UserInterface {
   @Test
-  public void f() {
-	  System.out.println("Hello");
+  public void f() throws InterruptedException {
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("https://demo.vaadin.com/sampler/#");
+	  Thread.sleep(2000);
+	  System.out.println(driver.findElement(By.xpath("(//div[@id='navigator']//div[@class='gwt-HTML titlelabel transitioned'])[1]")).getText());
+	  driver.quit();
   }
 }
